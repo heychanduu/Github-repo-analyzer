dd6110800ea32f5d6cc3a93e7af2438eb868fb3c
heychanduu <kushulurokula@outlook.com>
2026-03-16 15:16:08 +0530

    Remove dead Auth and JPA code causing compilation errors

--- src/main/java/com/github/portfolio/repository/TrackedProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.portfolio.repository;
-
-import com.github.portfolio.model.TrackedProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TrackedProfileRepository extends JpaRepository<TrackedProfile, Long> {
-    List<TrackedProfile> findByUserId(Long userId);
-}
